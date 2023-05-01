ecca07e4af5edb848fc41c467e83367b91dfd690
dabinchiii <dabinchi388@naver.com>
2023-05-01 20:24:37 +0900

    fix: Fix function call error

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
         elif operator == "-":
             minus(num1, num2)
         elif operator == "*":
-            print(multiplication(num1, num2))
+            print(multiplication.multiplication(num1, num2))
         elif operator == "/":
             plus(num1, num2)
             # 진우님 함수 호출
